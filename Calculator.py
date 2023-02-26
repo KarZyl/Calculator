0dffbb0e3ece8d13714791f1f22d75d548aaa367
KarZyl <125766453+KarZyl@users.noreply.github.com>
2023-02-26 20:14:19 +0100

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,61 +1,91 @@
 
 import logging
 logging.basicConfig(level=logging.INFO)
+check = 0
 
-
-a = (input("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: "))
-
-try: int(a)
-except ValueError:
-    logging.error(" Błąd! Podana wartość nie jest liczbą")
-
-a = int(a)
+operation = input("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: ")
+while check == 0:
+    try: 
+        int(operation)
+        check = 1
+    except ValueError:
+        check = 0
+        logging.error(" Błąd! Podana wartość nie jest liczbą")
+        operation = input("podaj nową wartość: ")
+operation = int(operation)
+check = 0
 
 number1 = input("Podaj składnik 1: " )
-try: int(number1)
-except ValueError:
-    logging.error(" Błąd! Podana wartość nie jest liczbą")
+while check == 0:
+    try: 
+        int(number1)
+        check = 1
+    except ValueError:
+        check = 0
+        logging.error(" Błąd! Podana wartość nie jest liczbą")
+        number1 = input("podaj nową wartość: ")
 number1 = int(number1)
+check = 0
 
 number2 = input("Podaj składnik 2: " )
-try: int(number2)
-except ValueError:
-    logging.error(" Błąd! Podana wartość nie jest liczbą")
+while check == 0:
+    try: 
+        int(number2)
+        check = 1
+    except ValueError:
+        check = 0
+        logging.error(" Błąd! Podana wartość nie jest liczbą")
+        number2 = input("podaj nową wartość: ")
 number2 = int(number2)
+check = 0
 
-if a == 1:
+if operation == 1:
     answer = input("Czy chcesz dodać więcej liczb? T/N ")
     numbers = [number1, number2]
     result = number1 + number2
     while answer == "T":
         number3 = input("Podaj kolejny składnik")
-        try: int(number3)
-        except ValueError:
-            logging.error(" Błąd! Podana wartość nie jest liczbą")
+        while check == 0:
+            try: 
+                int(number3)
+                check = 1
+            except ValueError:
+                logging.error(" Błąd! Podana wartość nie jest liczbą, podaj wartość liczbową")
+                check = 0
+                number3 = input("podaj nową wartość: ")
+        check = 0
         number3 = int(number3)
+
         result = result + number3
         numbers = numbers + [number3]
         answer = input("Czy chcesz dodać więcej liczb? T/N ")
 
     logging.info(f" Dodaję liczby: {numbers}")
-elif a == 2:
+elif operation == 2:
     result = number1 - number2
     logging.info(f" Odejmuję {number2} od {number1}")
-elif a == 3:
+elif operation == 3:
     answer = input("Czy chcesz pomnożyć więcej liczb? T/N")
     numbers = [number1, number2]
     result = number1 * number2
     while answer == "T":
         number3 = input("Podaj kolejny składnik")
-        try: int(number3)
-        except ValueError:
-            logging.error(" Błąd! Podana wartość nie jest liczbą")
+        while check == 0:
+            try: 
+                int(number3)
+                check = 1
+            except ValueError:
+                logging.error(" Błąd! Podana wartość nie jest liczbą")
+                check = 0
+                number3 = input("podaj nową wartość: ")
+        check = 0
         number3 = int(number3)
+
         result = result * number3
         numbers = numbers + [number3]
         answer = input("Czy chcesz pomnożyć więcej liczb? T/N")
     logging.info(f" Mnożę liczby: {numbers}")
-elif a == 4:
+elif operation == 4:
     result = number1 / number2
     logging.info(f" Dzielę {number1} przez {number2}")
 else:
