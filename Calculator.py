a445401c3c16c6e77f794af1f379960c12ed368a
KarZyl <125766453+KarZyl@users.noreply.github.com>
2023-02-28 19:43:34 +0100

    Update function, float,

--- Calculator.py
+++ Calculator.py
@@ -1,95 +1,73 @@
 
 import logging
 logging.basicConfig(level=logging.INFO)
-check = 0
 
-operation = input("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: ")
-while check == 0:
-    try: 
-        int(operation)
-        check = 1
-    except ValueError:
-        check = 0
-        logging.error(" Błąd! Podana wartość nie jest liczbą")
-        operation = input("podaj nową wartość: ")
-operation = int(operation)
-check = 0
+print(type(3.1))
 
-number1 = input("Podaj składnik 1: " )
-while check == 0:
-    try: 
-        int(number1)
-        check = 1
-    except ValueError:
-        check = 0
-        logging.error(" Błąd! Podana wartość nie jest liczbą")
-        number1 = input("podaj nową wartość: ")
-number1 = int(number1)
-check = 0
+print("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: ")
 
-number2 = input("Podaj składnik 2: " )
-while check == 0:
-    try: 
-        int(number2)
-        check = 1
-    except ValueError:
-        check = 0
-        logging.error(" Błąd! Podana wartość nie jest liczbą")
-        number2 = input("podaj nową wartość: ")
-number2 = int(number2)
-check = 0
+def input_try_int():
+    argument = input()
+    check = 0
+    while check == 0:
+        try: 
+            int(argument)
+            check = 1
+            argument = int(argument)
+        except ValueError:
+            try:
+                float(argument)
+                check = 1
+                argument = float(argument)
+            except ValueError:
+                check = 0
+                logging.error(" Błąd! Podana wartość nie jest liczbą")
+                argument = input("podaj nową wartość: ")
+    return argument
+
+operation = input_try_int()
+
+while operation != 1 and operation !=2 and operation !=3 and operation !=4:
+    print("Wybrano nieprawidłowe działanie, wybierz spośród liczb 1-4: ")
+    operation = input_try_int()
+
+print("Podaj składnik 1: ")
+
+number1 = input_try_int()
+
+print("Podaj składnik 2: ")
+
+number2 = input_try_int()
 
 if operation == 1:
-    answer = input("Czy chcesz dodać więcej liczb? T/N ")
+    answer = input("Wprowadź 'T' jeśli chcesz dodać więcej liczb: ")
     numbers = [number1, number2]
     result = number1 + number2
     while answer == "T":
-        number3 = input("Podaj kolejny składnik")
-        while check == 0:
-            try: 
-                int(number3)
-                check = 1
-            except ValueError:
-                logging.error(" Błąd! Podana wartość nie jest liczbą, podaj wartość liczbową")
-                check = 0
-                number3 = input("podaj nową wartość: ")
-        check = 0
-        number3 = int(number3)
-
+        print("Podaj kolejny składnik")
+        number3 = input_try_int()
         result = result + number3
         numbers = numbers + [number3]
-        answer = input("Czy chcesz dodać więcej liczb? T/N ")
+        answer = input("Wprowadź 'T' jeśli chcesz dodać więcej liczb: ")
 
     logging.info(f" Dodaję liczby: {numbers}")
 elif operation == 2:
     result = number1 - number2
     logging.info(f" Odejmuję {number2} od {number1}")
 elif operation == 3:
-    answer = input("Czy chcesz pomnożyć więcej liczb? T/N")
+    answer = input("Wprowadź 'T' jeśli chcesz pomnożyć więcej liczb: ")
     numbers = [number1, number2]
     result = number1 * number2
     while answer == "T":
-        number3 = input("Podaj kolejny składnik")
-        while check == 0:
-            try: 
-                int(number3)
-                check = 1
-            except ValueError:
-                logging.error(" Błąd! Podana wartość nie jest liczbą")
-                check = 0
-                number3 = input("podaj nową wartość: ")
-        check = 0
-        number3 = int(number3)
-
+        print("Podaj kolejny składnik")
+        number3 = input_try_int()
         result = result * number3
-        numbers = numbers + [number3]
-        answer = input("Czy chcesz pomnożyć więcej liczb? T/N")
+        answer = input("Wprowadź 'T' jeśli chcesz pomnożyć więcej liczb: ")
     logging.info(f" Mnożę liczby: {numbers}")
 elif operation == 4:
     result = number1 / number2
     logging.info(f" Dzielę {number1} przez {number2}")
 else:
-    result = None
-    print("Nie ma takiego działania")
+    None
 
 print(f"Wynik to {result}")
